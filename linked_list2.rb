1b3c9e3f80009bda56dae8abc741afec97547594
sheard93 <shelbyzaglin@gmail.com>
2019-10-30 21:58:38 +0000

    Initial Commit

---
+++ linked_list2.rb
@@ -0,0 +1,39 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
+end
+
+def reverse_list(list, previous=nil)
+  previous_node = list.next_node
+  list.next_node = previous
+  if previous_node
+    reverse_list(previous_node, list)
+  else
+    list
+  end
+end
+
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+print_values(node3)
+puts "-------"
+revlist = reverse_list(node3)
+print_values(revlist)
+
